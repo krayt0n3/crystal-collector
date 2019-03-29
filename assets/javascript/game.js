5dbf5f6d590732b7aa15bcd7e7f7a7ba7ce4d00d
Krayt0n3 <hendersonkevin64@gmail.com>
2019-03-29 10:39:55 -0500

    image

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -44,7 +44,7 @@ $(document).ready(function(){
 
       imageCrystal.addClass("crystal-image");
 
-      imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+      imageCrystal.attr("src", "https://github.com/krayt0n3/unit-4-game/blob/initial-site/assets/images/crystal.jpg");
 
       imageCrystal.attr("data-crystal-value", numberOptions[i]);
 
